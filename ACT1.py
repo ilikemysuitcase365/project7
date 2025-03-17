95b63f3e9a5cadc303610e426a43a7ca0a50fc63
ilikemysuitcase365 <karbaaa1234@gmail.com>
2025-03-17 17:16:05 +0800

    c

--- ACT1.py
+++ ACT1.py
@@ -1,6 +1,9 @@
-swapTHREE (int &A int &B int &C)
 
-A= A + B + C;
+A = 10 
+B = 20 
+C = 30
+
+A= A + B + C
 
 B = A - (B+C)
 
@@ -8,11 +11,7 @@ C = A - (B+C)
 
 A = A - (B+C)
 
-
-int main()
-
-int A = 10 B = 20 C = 30
- 
-cout << Before swapping A = "<< a << ", B = "
-<< B << ", C = " << C << endl;
+print(A)
+print(B)
+print(C) 
 
