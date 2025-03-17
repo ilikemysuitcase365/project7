c70777111d099909e4f89136ca6769f67987696c
ilikemysuitcase365 <karbaaa1234@gmail.com>
2025-03-17 17:07:54 +0800

    c

--- act2.py
+++ act2.py
@@ -1,8 +1,9 @@
 age = 40
 if age > 40:
     if age > 60:
-    print('turning old')
-else:
-    print('middle age')
+     print('turning old')
+    else:
+       print('middle aged')
+    
 else:
     print ('young age')
