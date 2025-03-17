139cdc848b751948a046d7dc5cf265b5f8e00174
ilikemysuitcase365 <karbaaa1234@gmail.com>
2025-03-17 16:25:25 +0800

    commit

---
+++ ACT1.py
@@ -0,0 +1,18 @@
+swapTHREE (int &A int &B int &C)
+
+A= A + B + C;
+
+B = A - (B+C)
+
+C = A - (B+C)
+
+A = A - (B+C)
+
+
+int main()
+
+int A = 10 B = 20 C = 30
+ 
+cout << Before swapping A = "<< a << ", B = "
+<< B << ", C = " << C << endl;
+
---
+++ act2.py
@@ -0,0 +1,8 @@
+age = 40
+if age > 40:
+    if age > 60:
+    print('turning old')
+else:
+    print('middle age')
+else:
+    print ('young age')
